1ad8d149a7c36094242e7d7f25c6c30600206ad5
deniseho <deniseho630@gmail.com>
2016-03-17 02:32:42 +0800

    修改讓第二個測試案例通過 (green)

--- HarryPotterTest/PotterShoppingCart/ShoppingCart.cs
+++ HarryPotterTest/PotterShoppingCart/ShoppingCart.cs
@@ -11,8 +11,25 @@ namespace PotterShoppingCart
         public double Checkout(List<Book> order)
         {
             var orderCount = order.Count();
+
+            var discount = GetDiscount(orderCount);
+
             var bookPrice = new Book().Price;
-            return orderCount * bookPrice;
+
+            return orderCount * bookPrice * discount;
+        }
+
+        private double GetDiscount(int orderCount)
+        {
+            switch (orderCount)
+            {
+                case 1:
+                    goto default;
+                case 2:
+                    return 0.95;
+                default:
+                    return 1;
+            }
         }
     }
 }
